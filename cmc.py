1ffb01dc555e39e9236c40b2773b7d8e22046a0b
eupotter <siteuriandrei@gmail.com>
2024-01-17 13:57:00 +0200

    Added a base template for HTML pages and a new navbar. Also added variables for page title and content

--- cmc.py
+++ cmc.py
@@ -5,28 +5,28 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return render_template("index.html")
+def main(title="Home", content="Welcome to CMC web app"):
+    return render_template("index.html", title=title, content=content)
 
 @app.route("/forum")
-def forum():
-    return render_template("forum.html")
+def forum(title="Forum", content="Welcome to CMC forum"):
+    return render_template("forum.html", title=title, content=content)
 
 @app.route("/donations")
-def donations():
-    return render_template("donations.html")
+def donations(title="Donations", content="Welcome to CMC donations"):
+    return render_template("donations.html", title=title, content=content)
 
 @app.route("/meetings")
-def meetings():
-    return render_template("meetings.html")
+def meetings(title="Meetings", content="Welcome to CMC meetings"):
+    return render_template("meetings.html", title=title, content=content)
 
 @app.route("/motorcycleclubs")
-def mcs():
-    return render_template("mcs.html")
+def mcs(title="MCs", content="Welcome to CMC page where MCs can present themselves"):
+    return render_template("mcs.html", title=title, content=content)
 
 @app.route("/about")
-def about():
-    return render_template("about.html")
+def about(title="About", content="Here you can learn about CMC"):
+    return render_template("about.html", title=title, content=content)
 
 if __name__ == "__main__":
     app.run()
